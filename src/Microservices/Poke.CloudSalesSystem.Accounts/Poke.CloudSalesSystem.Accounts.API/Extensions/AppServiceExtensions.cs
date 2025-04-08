b88c5f4c55ae6544aec61fea060dabf54aca667a
Slavisa Pokimica <slavisa.pokimica@pl.abb.com>
2025-04-08 03:18:49 +0200

    Adding resilience on startup migrations

--- src/Microservices/Poke.CloudSalesSystem.Accounts/Poke.CloudSalesSystem.Accounts.API/Extensions/AppServiceExtensions.cs
+++ src/Microservices/Poke.CloudSalesSystem.Accounts/Poke.CloudSalesSystem.Accounts.API/Extensions/AppServiceExtensions.cs
@@ -2,6 +2,7 @@
 using Poke.CloudSalesSystem.Accounts.Application;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Repository;
+using Polly;
 
 namespace Poke.CloudSalesSystem.Accounts.API.Extensions;
 
@@ -20,15 +21,21 @@ public static class AppServiceExtensions
     {
         using var scope = app.ApplicationServices.CreateScope();
         var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-        try
+
+        var retryPolicy = Policy.Handle<Npgsql.NpgsqlException>()
+            .WaitAndRetry(10,
+            attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt)), //exponential backoff
+            (exception, timeSpan, attempt, context) =>
+            {
+                logger.LogError($"Running DB migrations attempt {attempt} failed. Retrying in {timeSpan.TotalSeconds} seconds. Exception: {exception.Message}");
+            });
+
+        using var dbContext = scope.ServiceProvider.GetRequiredService<AccountsDbContext>();
+
+        retryPolicy.Execute(() =>
         {
-            //Add resilience 
-            using var dbContext = scope.ServiceProvider.GetRequiredService<AccountsDbContext>();
             dbContext.Database.Migrate();
-        }
-        catch(Exception ex)
-        {
-            logger.LogError(ex, $"Migration failed to db: {ex}");
-        }
+            logger.LogInformation($"Migrations completed");
+        });
     }
 }
--- src/Microservices/Poke.CloudSalesSystem.Customers/Poke.CloudSalesSystem.Customers.API/Extensions/AppServiceExtensions.cs
+++ src/Microservices/Poke.CloudSalesSystem.Customers/Poke.CloudSalesSystem.Customers.API/Extensions/AppServiceExtensions.cs
@@ -2,6 +2,7 @@
 using Poke.CloudSalesSystem.Customers.Application;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Repository;
+using Polly;
 
 namespace Poke.CloudSalesSystem.Customers.API.Extensions;
 
@@ -20,14 +21,21 @@ public static class AppServiceExtensions
     {
         using var scope = app.ApplicationServices.CreateScope();
         var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-        try
+
+        var retryPolicy = Policy.Handle<Npgsql.NpgsqlException>()
+            .WaitAndRetry(10,
+            attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt)), //exponential backoff
+            (exception, timeSpan, attempt, context) =>
+            {
+                logger.LogError($"Running DB migrations attempt {attempt} failed. Retrying in {timeSpan.TotalSeconds} seconds. Exception: {exception.Message}");
+            });
+
+        using var dbContext = scope.ServiceProvider.GetRequiredService<CustomersDbContext>();
+
+        retryPolicy.Execute(() =>
         {
-            using var dbContext = scope.ServiceProvider.GetRequiredService<CustomersDbContext>();
             dbContext.Database.Migrate();
-        }
-        catch(Exception ex)
-        {
-            logger.LogError(ex, $"Migration failed to db: {ex}");
-        }
+            logger.LogInformation($"Migrations completed");
+        });
     }
 }
